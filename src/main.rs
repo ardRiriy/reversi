9eae626a05dce208e652d404c20592db3a5edb52
ardririy <hynnkgw27@gmail.com>
2022-12-10 18:01:37 +0900

    define stone and field, and they can be printed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#[derive(Clone, Copy)]
+enum Stone {
+    Empty,
+    Black,
+    White,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut field: [[Stone; 10]; 10] = [[Stone::Empty; 10]; 10];
+    field[4][4] = Stone::Black;
+    field[4][5] = Stone::White;
+    field[5][4] = Stone::White;
+    field[5][5] = Stone::Black;
+
+    print_field(&field);
+}
+
+fn print_field(field: &[[Stone; 10]; 10]) {
+    println!("  A B C D E F G H I J");
+    let mut i = 0;
+
+    for row in field {
+        print!("{}", i);
+        for stone in row {
+            print!(" ");
+            match stone {
+                Stone::Empty => print!("."),
+                Stone::Black => print!("X"),
+                Stone::White => print!("O"),
+            }
+        }
+        println!("");
+        i += 1;
+    }
 }
