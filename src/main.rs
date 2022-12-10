8d251ee9d9d9a47ffd1ec7c0baefcdb0b5efd371
ardririy <hynnkgw27@gmail.com>
2022-12-10 18:24:14 +0900

    can put stone

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Stone {
     Empty,
     Black,
@@ -12,7 +12,43 @@ fn main() {
     field[5][4] = Stone::White;
     field[5][5] = Stone::Black;
 
+    loop {
+        let mut turn_count = 1;
+        }
+    }
+}
+
+fn turn(field: &[[Stone; 10]; 10], stone: Stone) {
     print_field(&field);
+    println!("Enter a move (ex: A1): ");
+    loop {
+        let (x, y) = get_input();
+        if is_valid_move(&field, x, y) {
+            field[y as usize][x as usize] = stone;
+            break;
+        } else {
+            println!("Can't place stone there or invaild input. Try again!");
+        }
+    }
+}
+
+fn get_input() -> (u8, u8) {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+    let x = input.chars().nth(0).unwrap() as u8 - 'A' as u8;
+    let y = input.chars().nth(1).unwrap() as u8 - '0' as u8;
+    (x, y)
+}
+
+fn is_valid_move(field: &[[Stone; 10]; 10], x: u8, y: u8) -> bool {
+    if x > 9 || y > 9 {
+        return false;
+    }
+    if field[y as usize][x as usize] != Stone::Empty {
+        return false;
+    }
+    true
 }
 
 fn print_field(field: &[[Stone; 10]; 10]) {
